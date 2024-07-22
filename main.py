730abca1cf1fa7d1774da364d8fa80c7bcec5066
dhiraj8396 <160827527+dhiraj8396@users.noreply.github.com>
2024-07-22 19:03:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+import streamlit as st
+
+st.title("Hello From StreamLit Page")
+st.header("Python")
+st.subheader("Java")
+st.markdown("I love python")
+st.code("""for i in range(1,5,1):
+    print("hello")
+""")
